894d7a2c1eb6e6f203868a63ef7374d026345d75
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-09-29 17:16:22 +0700

    TKW-80 <finish>

---
+++ src/components/clients/ServiceModal.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { Button, Modal } from "antd";
+import { Checkbox } from "antd";
+const ServiceModal = (props) => {
+  //  Modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+    ChildServiceID(dataUptoForm);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  // ----------------------------
+  const [service, setService] = useState();
+  let options = [];
+  service?.map((item) => options.push({ value: item._id, label: item.name }));
+
+  const [dataUptoForm, setdataUptoForm] = useState({});
+  const onChange = (checkedValues) => {
+    console.log("checked = ", checkedValues);
+    setdataUptoForm(checkedValues)
+  };
+
+  const ChildServiceID = (e) => {
+    props.ParentServiceId(e);
+  };
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/service`)
+      .then((response) => response.json())
+      .then((data) => setService(data));
+  }, []);
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Checkbox.Group options={options} onChange={onChange} />
+      </Modal>
+    </>
+  );
+};
+
+export default ServiceModal;

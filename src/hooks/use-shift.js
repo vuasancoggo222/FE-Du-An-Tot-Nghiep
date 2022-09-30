dce4204f472f49e76ab18f3779c8f3e0718a1534
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-09-30 22:06:38 +0700

    TKW-90 <create swr general function>

---
+++ src/hooks/use-shift.js
@@ -0,0 +1,35 @@
+import useSWR from "swr";
+import instance from "../api/instance";
+import * as method from "../api/services";
+
+const fetcher = async (url) => await instance.get(url);
+
+const endpoint = "/shift";
+
+const useShift = () => {
+  const { data, error, mutate } = useSWR(endpoint, fetcher, {
+    refreshInterval: 5000,
+  });
+  const create = async (data) => {
+    const revalidate = await method.httpPost(endpoint, data);
+    mutate(revalidate);
+  };
+  const remove = async (id) => {
+    await method.httpDelete(endpoint, id);
+    const revalidate = data.filter((item) => item.id !== id);
+    mutate(revalidate);
+  };
+  const update = async (id, data) => {
+    const newData = await method.httpPut(endpoint, id, data);
+    const revalidate = data.map((item) => (item.id == id ? newData : item));
+    mutate(revalidate);
+  };
+  return {
+    data,
+    error,
+    create,
+    remove,
+    update,
+  };
+};
+export default useShift;

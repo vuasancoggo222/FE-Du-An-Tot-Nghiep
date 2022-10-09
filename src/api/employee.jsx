e15d331b6107d7f5091c18825cbe75d5cf834c8f
tamltph13479 <97274096+tamltph13479@users.noreply.github.com>
2022-10-09 14:10:52 +0700

    fix-banch-tkw-92

--- src/api/employee.jsx
+++ src/api/employee.jsx
@@ -8,12 +8,22 @@ export const getEmployeeByDate = (date, id) => {
 export const httpAddShift = (id, data) => {
   return instance.patch(`employees/new-employee-shift/${id}`, data);
 };
+
+export const httpUpdateEmployee = (id, data) => {
+  return instance.patch(`employees/${id}`, data);
+};
 export const httpGetOne = (id) => {
   return instance.get(`employees/${id}`);
 };
 
 export const httpGetEmployees = () => {
   return instance.get(`employees`);
+};
+export const httpAddEmployees = (data) => {
+  return instance.post(`employees`, data);
+};
+export const httpUpdateEmployees = (id, data) => {
+  return instance.put(`employees/${id}`, data);
 };
 
 export const httpChangeStatusTimeWork = (id, date, shift, data) => {
@@ -24,16 +34,6 @@ export const httpChangeStatusTimeWork = (id, date, shift, data) => {
 };
 
 export const removeEmployees = (id, data) => {
-  const url = `employees/${id}`;
-  return instance.delete(url, data);
-};
-
-export const httpAddEmployees = (data) => {
-  const url = `employees`;
-
-  return instance.post(url, data);
-};
-export const httpUpdateEmployees = (id, data) => {
   const url = `employees/${id}`;
   return instance.delete(url, data);
 };

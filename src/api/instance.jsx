32fe63eb721304f891993aca3e9c50ed375de77a
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-10-22 12:31:54 +0700

    fix bug & role admin

--- src/api/instance.jsx
+++ src/api/instance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://192.168.1.3:5000/api",
+  baseURL: "http://localhost:5000/api",
 });
 instance.interceptors.response.use(
   function (response) {

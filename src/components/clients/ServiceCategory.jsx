1ce299eb593375e5bec72f77cda801c7f7eb69ec
trung9901 <79823094+trung9901@users.noreply.github.com>
2022-11-18 14:33:33 +0700

    Update ServiceCategory.jsx

--- src/components/clients/ServiceCategory.jsx
+++ src/components/clients/ServiceCategory.jsx
@@ -1,28 +1,30 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { httpGetAllService } from "../../api/services";
 import useService from "../../hooks/use-service";
 const ServiceCategory = () => {
-  const { data, error } = useService();
   const [service, setService] = React.useState([]);
 
-  if (!data) return <div>Loading...</div>;
-  if (error) return <div>Something went wrong</div>;
   useEffect(() => {
-    setService(data.filter((item) => item.status !== 2));
+    const service = async () => {
+      const res = await httpGetAllService();
+      setService(res.filter((item) => item.status !== 2));
+    };
+    service();
   }, []);
   return (
     <>
       <div className="grid grid-cols-[190px_190px_190px] ml-16">
-        {service.map((item) => (
-          <div key={item._id}>
-            <Link to={`/detail-booking/${item.slug}`}>
+        {service?.map((item) => (
+          <div key={item?._id}>
+            <Link to={`/detail-booking/${item?.slug}`}>
               <a>
                 <img
                   src="https://beautyspa4.shostweb.com/wp-content/uploads/2021/11/f2-small-dv1.jpg"
                   alt=""
                 />
                 <div className="text-center py-[10px]">
-                  <button>{item.name}</button>
+                  <button>{item?.name}</button>
                 </div>
               </a>
             </Link>

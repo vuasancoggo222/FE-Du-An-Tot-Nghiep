429c38c3937aac4f3b6fe6dd507bdf3414ff25a5
tamltph13479 <97274096+tamltph13479@users.noreply.github.com>
2022-12-21 13:09:33 +0700

    fix-api-bookingadd

--- src/api/booking.jsx
+++ src/api/booking.jsx
@@ -1,28 +1,28 @@
 import instance from "./instance";
 import { isAuthenticate } from "../utils/LocalStorage";
 
-const user = isAuthenticate()
-let header = {}
-if(user){
-header = {
-  headers: {
-    Authorization: `${user.token}`,
-  },
-}
+const user = isAuthenticate();
+let header = {};
+if (user) {
+  header = {
+    headers: {
+      Authorization: `${user.token}`,
+    },
+  };
 }
 
 const httpGetAll = () => {
-  return instance.get(`booking`,header);
+  return instance.get(`booking`, header);
 };
 
 const bookingGenderStatistics = () => {
-  const url = `booking-gender-statistics`
-  return instance.get(url,header);
+  const url = `booking-gender-statistics`;
+  return instance.get(url, header);
 };
-const httpAddBooking = (token, data,userId) => {
-  let userID = userId
-  if(!userID){
-    userID = "637e321c347223cf109f85e3"
+const httpAddBooking = (token, data, userId) => {
+  let userID = userId;
+  if (!userID) {
+    userID = "637e321c347223cf109f85e3";
   }
   const header = {
     headers: {
@@ -32,19 +32,23 @@ const httpAddBooking = (token, data,userId) => {
   return instance.post(`booking?user=${userID}`, data, header);
 };
 const bookingAddByEmployeeApi = (data) => {
-  return instance.post(`bookingAddByEmployee`, data,header); 
+  let userID = "";
+  if (!userID) {
+    userID = "637e321c347223cf109f85e3";
+  }
+  return instance.post(`bookingAddByEmployee?user=${userID}`, data, header);
 };
 const httpGetOne = (id) => {
-  return instance.get(`booking/${id}`,header);
+  return instance.get(`booking/${id}`, header);
 };
 const httpGetChangeStatus = (id, data) => {
-  return instance.patch(`booking/${id}`, data,header);
+  return instance.patch(`booking/${id}`, data, header);
 };
 
 export const userHistory = (id) => {
-  const url = `booking-history/${id}`
-  return instance.get(url,header)
-}
+  const url = `booking-history/${id}`;
+  return instance.get(url, header);
+};
 // const httpPut = (endpoint, id, data) => {
 //   return instance.put(`${endpoint}/${id}`, data);
 // };
@@ -53,10 +57,14 @@ export const userHistory = (id) => {
 //   return instance.delete(`${endpoint}/${id}`);
 // };
 export const bookingForEmployee = () => {
-  
-  return instance.get(`/booking-employee-list/${user.employeeId}`,header)
-}
+  return instance.get(`/booking-employee-list/${user.employeeId}`, header);
+};
 export {
-  httpGetAll, httpAddBooking, httpGetOne, httpGetChangeStatus, bookingGenderStatistics, bookingAddByEmployeeApi
-  //  httpPut, httpDelete 
+  httpGetAll,
+  httpAddBooking,
+  httpGetOne,
+  httpGetChangeStatus,
+  bookingGenderStatistics,
+  bookingAddByEmployeeApi,
+  //  httpPut, httpDelete
 };

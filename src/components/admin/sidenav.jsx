3f90db705099bc9e97d20812c99ca7756bf462a9
tamltph13479 <97274096+tamltph13479@users.noreply.github.com>
2022-12-23 22:07:57 +0700

    Active-sidenav

--- src/components/admin/sidenav.jsx
+++ src/components/admin/sidenav.jsx
@@ -2,6 +2,164 @@ import { message } from "antd";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { isAuthenticate } from "../../utils/LocalStorage";
+
+import { Menu } from "antd";
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+        to={`/admin`}
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img
+            width="100%"
+            src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-dashboard-mobile-app-development-flaticons-lineal-color-flat-icons-4.png"
+          />
+        </div>
+        <span className="">Thống kê</span>
+      </Link>
+    </li>,
+    "1"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        to="/admin/employee"
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-spa-travel-agency-flaticons-lineal-color-flat-icons.png" />
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Nhân viên
+        </span>
+      </Link>
+    </li>,
+    "2"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        to="/admin/booking"
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-booking-spa-flaticons-lineal-color-flat-icons.png" />{" "}
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Đặt lịch
+        </span>
+      </Link>
+    </li>,
+    "3"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+        to="/admin/service"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-spa-hospitality-services-flaticons-lineal-color-flat-icons.png" />{" "}
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Dịch vụ
+        </span>
+      </Link>
+    </li>,
+    "4"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+        to="/admin/contact"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/cotton/64/null/contact-card.png" />{" "}
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Liên hệ
+        </span>
+      </Link>
+    </li>,
+    "5"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+        to="/admin/banner"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/external-smashingstocks-isometric-smashing-stocks/55/null/external-Banner-sports-smashingstocks-isometric-smashing-stocks-2.png" />{" "}
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Ảnh Banner
+        </span>
+      </Link>
+    </li>,
+    "6"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+        to="/admin/post"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/nolan/64/us-news.png" />{" "}
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Bài viết
+        </span>
+      </Link>
+    </li>,
+    "7"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+        to="/admin/voucher"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/external-smashingstocks-isometric-smashing-stocks/55/null/external-Banner-sports-smashingstocks-isometric-smashing-stocks-2.png" />{" "}
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Khuyến mại
+        </span>
+      </Link>
+    </li>,
+    "8"
+  ),
+  getItem(
+    <li className="mt-0.5 w-full">
+      <Link
+        to="/admin/user"
+        className=" flex items-center whitespace-nowrap px-4 transition-colors"
+      >
+        <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+          <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-spa-alternative-medicine-flaticons-lineal-color-flat-icons.png" />{" "}
+        </div>
+        <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
+          Tài khoản
+        </span>
+      </Link>
+    </li>,
+    "9"
+  ),
+];
 const Sidenav = () => {
   const navigate = useNavigate();
   const user = isAuthenticate();
@@ -64,131 +222,15 @@ const Sidenav = () => {
             </ul>
           ) : (
             <ul className="flex flex-col pl-0 mb-0">
-              <li className="mt-0.5 w-full">
-                <Link
-                  className="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-                  to={`/admin`}
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                    <img
-                      width="100%"
-                      src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-dashboard-mobile-app-development-flaticons-lineal-color-flat-icons-4.png"
-                    />
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Thống kê
-                  </span>
-                </Link>
-              </li>
-              <li className="mt-0.5 w-full">
-                <Link
-                  to="/admin/employee"
-                  className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                    <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-spa-travel-agency-flaticons-lineal-color-flat-icons.png" />
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Nhân viên
-                  </span>
-                </Link>
-              </li>
-              <li className="mt-0.5 w-full">
-                <Link
-                  to="/admin/booking"
-                  className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
-                    <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-booking-spa-flaticons-lineal-color-flat-icons.png" />{" "}
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Đặt lịch
-                  </span>
-                </Link>
-              </li>
-              <li className="mt-0.5 w-full">
-                <Link
-                  className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                  to="/admin/service"
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                    <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-spa-hospitality-services-flaticons-lineal-color-flat-icons.png" />{" "}
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Dịch vụ
-                  </span>
-                </Link>
-              </li>
-              <li className="mt-0.5 w-full">
-                <Link
-                  className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                  to="/admin/contact"
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                    <img src="https://img.icons8.com/cotton/64/null/contact-card.png" />{" "}
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Liên hệ
-                  </span>
-                </Link>
-              </li>
-              <li className="mt-0.5 w-full">
-                <Link
-                  className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                  to="/admin/banner"
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                    <img src="https://img.icons8.com/external-smashingstocks-isometric-smashing-stocks/55/null/external-Banner-sports-smashingstocks-isometric-smashing-stocks-2.png" />{" "}
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Ảnh Banner
-                  </span>
-                </Link>
-              </li>
-              <li className="mt-0.5 w-full">
-                <Link
-                  className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                  to="/admin/post"
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                    <img src="https://img.icons8.com/nolan/64/us-news.png" />{" "}
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Bài viết
-                  </span>
-                </Link>
-                <li className="mt-0.5 w-full">
-                  <Link
-                    className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                    to="/admin/voucher"
-                  >
-                    <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                      <img src="https://img.icons8.com/external-smashingstocks-isometric-smashing-stocks/55/null/external-Banner-sports-smashingstocks-isometric-smashing-stocks-2.png" />{" "}
-                    </div>
-                    <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                      Khuyến mại
-                    </span>
-                  </Link>
-                </li>
-              </li>
-              <li className="w-full mt-4">
-                <h6 className="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">
-                  Tài khoản
-                </h6>
-              </li>
-              <li className="mt-0.5 w-full">
-                <Link
-                  to="/admin/user"
-                  className=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                >
-                  <div className="mr-2 flex h-8 w-10 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                    <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/null/external-spa-alternative-medicine-flaticons-lineal-color-flat-icons.png" />{" "}
-                  </div>
-                  <span className="ml-1 duration-300 opacity-100 pointer-events-none ease">
-                    Tài khoản
-                  </span>
-                </Link>
-              </li>
+              <div className="">
+                <Menu
+                  defaultSelectedKeys={["1"]}
+                  defaultOpenKeys={["sub1"]}
+                  mode="vertical"
+                  theme="light"
+                  items={items}
+                />
+              </div>
               <li className="mt-10 grid">
                 <button
                   type="button"
